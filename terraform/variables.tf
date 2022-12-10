733dbfd495ca80486b46ab1b604f70c575a49d0d
piasta <piastapiotr010@gmail.com>
2022-12-10 16:06:31 +0100

    Zad. 3 Terraform init/plan/apply/destroy

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "bucket_name" {
+  description = "doniczka"
+  type        = string
+}
+
+variable "tags" {
+  description = "Piotr Piasta: 75886"
+  type        = map(string)
+  default     = {}
+}
